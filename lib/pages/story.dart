d7ebfd8d74861062b41063ee803fd5bd3561b8fd
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2025-01-05 15:26:12 -0600

    handle nulls and change jokes

--- lib/pages/story.dart
+++ lib/pages/story.dart
@@ -24,7 +24,9 @@ class WinRateSlide extends StatelessWidget {
 
   Widget createWinRateSlide(double winRate) {
     Color textColor = winRate >= 50 ? Colors.green[300]! : Colors.red[300]!;
-    String message = winRate >= 50 ? "You're smashing it! 🚀" : "Got some work to do! 📉";
+    String message = winRate >= 50
+        ? "WAGMI! Your diamond hands are paying off! 💎🤲"
+        : "NGMI? Maybe it's time for some yield farming to moon again! 🌙";
 
     return Container(
       padding: const EdgeInsets.all(20),
@@ -35,12 +37,12 @@ class WinRateSlide extends StatelessWidget {
         children: [
           Text(
             "Win Rate: ${winRate.toStringAsFixed(2)}%",
-            style: TextStyle(color: textColor, fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           Text(
             message,
-            style: TextStyle(color: textColor, fontSize: 20),
+            style: TextStyle(color: textColor, fontSize: 24),
           ),
         ],
       ),
@@ -55,32 +57,50 @@ class WinRateGifDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // URLs for the GIFs
-    final String positiveGifUrl =
+    const String positiveGifUrl =
         "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHB5NW1yeTJoaHdoazU5dDg3Z3UxcHc0aHVyNThtdTF5cXhiNzhsdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/aRRGY0316l5GMK2uZE/giphy.webp";
-    final String negativeGifUrl =
+    const String negativeGifUrl =
         "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDQ3bnR2Y2QyazM4NWJ1OWs4bjZwOWk4czBlcDk2NHlybnI3ZXM4ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/IlgcQyV59WCWMgRI2D/giphy.webp";
 
-    // Determine which GIF to use based on the win rate
     String gifUrl = winRate >= 50 ? positiveGifUrl : negativeGifUrl;
+    String message =
+        winRate >= 50 ? "Green big dildos 🍆" : "You're cooked, my friend. Time to put the fries in the bag! 🍟";
 
     return Center(
-      child: Image.network(
-        gifUrl,
-        fit: BoxFit.cover,
-        loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-          if (loadingProgress == null) return child;
-          return Center(
-            child: CircularProgressIndicator(
-              value: loadingProgress.expectedTotalBytes != null
-                  ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
-                  : null,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.network(
+            gifUrl,
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.8,
+            // Adjust as needed
+            loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                      : null,
+                ),
+              );
+            },
+            errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+              return const Text('Failed to load image');
+            },
+          ),
+          SizedBox(height: 10), // Spacing between the GIF and text
+          Text(
+            message,
+            style: TextStyle(
+              color: winRate >= 50 ? Colors.green : Colors.red,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
-          );
-        },
-        errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-          return Text('Failed to load image');
-        },
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
@@ -97,12 +117,14 @@ class PnLWrappedSlide extends StatelessWidget {
   });
 
   String _getJokeBasedOnPnL() {
-    if (pnlPercent > 0) {
-      return "Looks like you're out of the trenches and heading to the moon! 🚀";
+    if (pnlPercent > 50) {
+      return "You're aping in like a true SOL degen! Wen Lambo? 🚀🚗";
+    } else if (pnlPercent > 0 && pnlPercent <= 50) {
+      return "Holding with diamond hands, but maybe you need some more hopium to moon! 💎🤲🌙";
     } else if (pnlPercent == 0) {
-      return "Steady in the trenches, soldier! Not every day's a battle. 🛡️";
+      return "No gains, no losses - you've just survived another FUD cycle! 🛡️";
     } else {
-      return "Might need to report back to retardio bootcamp! Better luck next flip! 🍔";
+      return "Looks like you've been hit by a rug pull or just caught the wrong meme coin. Time to farm some yield or wait for the next airdrop? 🧹🔄";
     }
   }
 
@@ -117,7 +139,7 @@ class PnLWrappedSlide extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
+        const Text(
           'Your PnL over the last month',
           style: TextStyle(
             color: Colors.white,
@@ -136,15 +158,15 @@ class PnLWrappedSlide extends StatelessWidget {
           pnlAmountDisplay,
           style: TextStyle(
             color: textColor,
-            fontSize: 20,
+            fontSize: 24,
           ),
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         Text(
           jokeText,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.white,
-            fontSize: 24,
+            fontSize: 22,
             fontStyle: FontStyle.italic,
           ),
         ),
@@ -158,19 +180,23 @@ class _StoryPageState extends State<StoryPage> {
 
   @override
   Widget build(BuildContext context) {
+    var winRate = widget.walletData.winrate ?? 0.0;
+
+    var pnln30d = widget.walletData.pnl ?? 0.0;
+
+    var totalProfit = widget.walletData.totalProfit ?? 0.0;
+
     return Scaffold(
         body: SizedBox(
             height: double.infinity,
             width: double.infinity,
             child: StoryView(controller: controller, repeat: true, storyItems: [
-              _storyItemInline(
-                  widget: WinRateSlide(winRate: widget.walletData.winrate! * 100), backgroundColor: Colors.black),
-              _storyItemInline(
-                  widget: WinRateGifDisplay(winRate: widget.walletData.winrate! * 100), backgroundColor: Colors.black),
+              _storyItemInline(widget: WinRateSlide(winRate: winRate * 100), backgroundColor: Colors.black),
+              _storyItemInline(widget: WinRateGifDisplay(winRate: winRate * 100), backgroundColor: Colors.black),
               _storyItemInline(
                   widget: PnLWrappedSlide(
-                    pnlPercent: widget.walletData.pnl30d! * 100 ,
-                    pnlAmount: widget.walletData.totalProfit!, // Appropriately humorous or motivational text
+                    pnlPercent: pnln30d * 100,
+                    pnlAmount: totalProfit, // Appropriately humorous or motivational text
                   ),
                   backgroundColor: Colors.black),
             ])));
@@ -187,12 +213,12 @@ class _StoryPageState extends State<StoryPage> {
         key: key,
         decoration: BoxDecoration(
           color: backgroundColor,
-          borderRadius: BorderRadius.vertical(
+          borderRadius: const BorderRadius.vertical(
             top: Radius.circular(0),
             bottom: Radius.circular(0),
           ),
         ),
-        padding: EdgeInsets.symmetric(
+        padding: const EdgeInsets.symmetric(
           horizontal: 24,
           vertical: 16,
         ),
@@ -202,7 +228,7 @@ class _StoryPageState extends State<StoryPage> {
         //color: backgroundColor,
       ),
       shown: true,
-      duration: duration ?? Duration(seconds: 5),
+      duration: duration ?? const Duration(seconds: 5),
     );
   }
 }
